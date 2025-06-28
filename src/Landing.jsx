d07d5de95043563a370f2cc12b3d28d8ff31b271
Saturno-0 <rst020514@gmail.com>
2025-06-28 00:38:38 -0600

    Adjust text size in Landing component for improved readability

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -52,8 +52,8 @@ function Landing() {
         className="fixed inset-0 object-cover h-screen md:w-[70%] md:mr-[40%] md:mr-auto"
       />
       <div className="hidden md:flex bg-gradient-to-r from-transparent from-60% to-[#282828] w-10 z-10"></div>
-      <div className="bg-black/30 backdrop-blur-sm rounded-3xl md:rounded-none absolute text-xl md:relative md:my-0 md:items-end self-center mx-15 md:mx-0 md:self-auto md:flex md:bg-[#282828] md:p-15 md:w-1/3 pb-5 px-5">
-        <p className="md:font-pesada font-media md:m-0 m-5">
+      <div className="bg-black/30 backdrop-blur-sm rounded-3xl md:rounded-none absolute text-md md:text-xl md:relative md:my-0 md:items-end self-center mx-15 md:mx-0 md:self-auto md:flex md:bg-[#282828] md:p-15 md:w-1/3 pb-5 px-5">
+        <p className="md:font-pesada  md:m-0 m-5">
           Argüende no se limita a ser una cafetería; es un espacio diseñado para
           capturar la esencia del ritual diario, destacando la simplicidad y
           autenticidad en cada taza.
