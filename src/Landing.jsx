64af316e1d0ceb9f5c0ac859441669b65c94eb12
Saturno-0 <rst020514@gmail.com>
2025-06-28 00:41:20 -0600

    changed file: src/Landing.jsx

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -53,7 +53,7 @@ function Landing() {
       />
       <div className="hidden md:flex bg-gradient-to-r from-transparent from-60% to-[#282828] w-10 z-10"></div>
       <div className="bg-black/30 backdrop-blur-sm rounded-3xl md:rounded-none absolute text-md md:text-xl md:relative md:my-0 md:items-end self-center mx-15 md:mx-0 md:self-auto md:flex md:bg-[#282828] md:p-15 md:w-1/3 pb-5 px-5">
-        <p className="md:font-pesada  md:m-0 m-5">
+        <p className=" text-center md:text-start md:m-0 m-2">
           Argüende no se limita a ser una cafetería; es un espacio diseñado para
           capturar la esencia del ritual diario, destacando la simplicidad y
           autenticidad en cada taza.
